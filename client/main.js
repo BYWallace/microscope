fa33ca835719d22442d4420d5e8cca21aa7c015b
BYWallace <bwallace90@gmail.com>
2015-02-19 20:24:09 -0500

    Configuration for Mongol

--- client/main.js
+++ client/main.js
@@ -1,1 +1,8 @@
 Meteor.subscribe('posts');
+
+Session.set("Mongol", {
+  'collections': ['Posts', 'Comments', 'Notifications'],
+  'display': true,
+  'opacity_normal': ".7",
+  'opacity_expand': ".9",
+});
