7770406bf29f573774fdd8c1c428581941aaa797
Brett Wallace <bwallace90@gmail.com>
2015-02-23 21:07:26 -0500

    Add getClicks method

--- packages/bitly/bitly.js
+++ packages/bitly/bitly.js
@@ -2,7 +2,7 @@ Bitly = {};
 
 Bitly.shortenURL = function(url) {
 	if(!Meteor.settings.bitly) {
-		throw new Meteor.Error(500, 'Please provide a Bitly token in Meteor.settings')
+		throw new Meteor.Error(500, 'Please provide a Bitly token in Meteor.settings');
 	}
 
 	var shortenResponse = Meteor.http.get(
@@ -23,3 +23,29 @@ Bitly.shortenURL = function(url) {
 		throw new Meteor.Error(500, 'Bitly call failed with error: ' + shortenResponse.status_txt);
 	}
 }
+
+Bitly.getClicks = function(link) {
+	if(!Meteor.settings.bitly) {
+		throw new Meteor.Error(500, 'Please provide a Bitly token in Meteor.settings');
+	}
+
+	var statsResponse = Meteor.http.get(
+		'https://api-ssl.bitly.com/v3/link/clicks?',
+		{
+			timeout: 5000,
+			params: {
+				'format': 'json',
+				'access_token': Meteor.settings.bitly,
+				'link': link
+			}
+		}
+	);
+	if(statsResponse.data.status_code === 200) {
+		return statsResponse.data.data.link_clicks
+	}
+}
+Meteor.methods({
+	'getBitlyClicks': function(link) {
+		return Bitly.getClicks(link);
+	}
+});
