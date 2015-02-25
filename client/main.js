7edda787aa276f907be5648e854de0465c68f16c
BYWallace <bwallace90@gmail.com>
2015-02-25 08:00:56 -0500

    Add Intercom code snippet

--- client/main.js
+++ client/main.js
@@ -6,3 +6,13 @@ Session.set("Mongol", {
   'opacity_normal': ".7",
   'opacity_expand': ".9",
 });
+
+window.intercomSettings = {
+  // TODO: The current logged in user's full name
+  name: 'John Doe',
+  // TODO: The current logged in user's email address
+  email: 'john.doe@example.com',
+  // TODO: The current logged in user's sign-up date as a Unix timestamp
+  created_at: 1234567890,
+  app_id: 'n5mvjfnv'
+};
